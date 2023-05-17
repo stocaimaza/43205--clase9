2c848ccedc46badc65b3b5266be9b60f3bea5d06
stocaimaza <stocaimaza@hotmail.com>
2023-05-17 12:46:02 -0300

    clase 9

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+import ItemList from '../ItemList/ItemList'
+import { getProductos, getProductosPorCategoria } from '../../asyncmock'
+import { useParams } from 'react-router-dom'
+
+const ItemListContainer = ({ greeting }) => {
+  const [productos, setProductos] = useState([]);
+
+  const { idCategoria } = useParams();
+
+  useEffect(() => {
+
+    const funcionProductos = idCategoria ? getProductosPorCategoria : getProductos;
+
+    funcionProductos(idCategoria)
+      .then(res => setProductos(res))
+      .catch(error => console.error(error))
+  }, [idCategoria])
+
+  return (
+    <>
+      <h2> {greeting} </h2>
+      <ItemList productos={productos} />
+    </>
+  )
+}
+
+export default ItemListContainer
